032edcdb2e84d6db2c24666d7be1f852f7728604
d22samma <d22samma@student.his.se>
2023-04-24 18:42:56 +0200

    Buttons Works in Facts.java

--- app/src/main/java/com/example/diaapp/Facts.java
+++ app/src/main/java/com/example/diaapp/Facts.java
@@ -2,7 +2,9 @@ package com.example.diaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Facts extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Facts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facts);
+
+        Button Type1 = findViewById(R.id.facts_type1btn);
+        Type1.setOnClickListener(view -> {
+            Intent intent = new Intent(Facts.this, Type1.class);
+            startActivity(intent);
+        });
+
+        Button Type2 = findViewById(R.id.facts_type2btn);
+        Type2.setOnClickListener(view -> {
+            Intent intent = new Intent(Facts.this, Type2.class);
+            startActivity(intent);
+        });
     }
 }
