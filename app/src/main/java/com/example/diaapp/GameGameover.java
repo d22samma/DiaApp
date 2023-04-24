d5acbc0c52c98a235a0e542bad9c469b04e8bc83
d22samma <d22samma@student.his.se>
2023-04-24 16:21:30 +0200

    Add Gameover Screen

---
+++ app/src/main/java/com/example/diaapp/GameGameover.java
@@ -0,0 +1,14 @@
+package com.example.diaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameGameover extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_gameover);
+    }
+}
