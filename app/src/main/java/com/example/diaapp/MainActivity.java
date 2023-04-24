704704a94d1f123a6de3a3def2e71a6e04386a3a
d22samma <d22samma@student.his.se>
2023-04-24 18:17:19 +0200

    Buttons Works in MainActivity.java

--- app/src/main/java/com/example/diaapp/MainActivity.java
+++ app/src/main/java/com/example/diaapp/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.diaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button factsbutton = findViewById(R.id.main_factsbtn);
+        factsbutton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Facts.class);
+            startActivity(intent);
+        });
+
+        Button gamebutton = findViewById(R.id.main_gamebtn);
+        gamebutton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, GAME.class);
+            startActivity(intent);
+            setContentView(R.layout.activity_main);
+        });
 
     }
 }
