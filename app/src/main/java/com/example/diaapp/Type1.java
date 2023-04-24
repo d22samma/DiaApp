0ce4df48e527ff59f4d7e425efaed6d29f2cd3da
d22samma <d22samma@student.his.se>
2023-04-24 19:30:11 +0200

    Added Buttons inside Type1.java and Type2.java

--- app/src/main/java/com/example/diaapp/Type1.java
+++ app/src/main/java/com/example/diaapp/Type1.java
@@ -2,7 +2,9 @@ package com.example.diaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Type1 extends AppCompatActivity {
 
@@ -10,5 +12,35 @@ public class Type1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_type1);
+
+        Button btn1 = findViewById(R.id.type2_btn);
+        btn1.setOnClickListener(view -> {
+            Intent intent = new Intent(Type1.this, ResponsiveFacts.class);
+            startActivity(intent);
+        });
+
+        Button btn2 = findViewById(R.id.type2_btn2);
+        btn2.setOnClickListener(view -> {
+            Intent intent = new Intent(Type1.this, ResponsiveFacts.class);
+            startActivity(intent);
+        });
+
+        Button btn3 = findViewById(R.id.type2_btn3);
+        btn3.setOnClickListener(view -> {
+            Intent intent = new Intent(Type1.this, ResponsiveFacts.class);
+            startActivity(intent);
+        });
+
+        Button btn4 = findViewById(R.id.type2_btn4);
+        btn4.setOnClickListener(view -> {
+            Intent intent = new Intent(Type1.this, ResponsiveFacts.class);
+            startActivity(intent);
+        });
+
+        Button btn5 = findViewById(R.id.type2_btn5);
+        btn5.setOnClickListener(view -> {
+            Intent intent = new Intent(Type1.this, ResponsiveFacts.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/diaapp/Type2.java
+++ app/src/main/java/com/example/diaapp/Type2.java
@@ -2,7 +2,9 @@ package com.example.diaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Type2 extends AppCompatActivity {
 
@@ -10,5 +12,35 @@ public class Type2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_type2);
+
+        Button btn1 = findViewById(R.id.type2_btn);
+        btn1.setOnClickListener(view -> {
+            Intent intent = new Intent(Type2.this, ResponsiveFacts2.class);
+            startActivity(intent);
+        });
+
+        Button btn2 = findViewById(R.id.type2_btn2);
+        btn2.setOnClickListener(view -> {
+            Intent intent = new Intent(Type2.this, ResponsiveFacts2.class);
+            startActivity(intent);
+        });
+
+        Button btn3 = findViewById(R.id.type2_btn3);
+        btn3.setOnClickListener(view -> {
+            Intent intent = new Intent(Type2.this, ResponsiveFacts2.class);
+            startActivity(intent);
+        });
+
+        Button btn4 = findViewById(R.id.type2_btn4);
+        btn4.setOnClickListener(view -> {
+            Intent intent = new Intent(Type2.this, ResponsiveFacts2.class);
+            startActivity(intent);
+        });
+
+        Button btn5 = findViewById(R.id.type2_btn5);
+        btn5.setOnClickListener(view -> {
+            Intent intent = new Intent(Type2.this, ResponsiveFacts2.class);
+            startActivity(intent);
+        });
     }
 }
