35c5201ad6e4a5d7e3f1bb602c1d6720edc9f2c4
d22samma <d22samma@student.his.se>
2023-04-25 07:26:43 +0200

    Added Intent between Type2.java and ResponsiveFacts2.java

--- app/src/main/java/com/example/diaapp/ResponsiveFacts2.java
+++ app/src/main/java/com/example/diaapp/ResponsiveFacts2.java
@@ -3,6 +3,7 @@ package com.example.diaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResponsiveFacts2 extends AppCompatActivity {
 
@@ -11,6 +12,17 @@ public class ResponsiveFacts2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_responsive_facts2);
 
+        TextView collectvalue = (TextView) findViewById(R.id.resp_title2);
+        TextView collectval = (TextView) findViewById(R.id.resp_desc2);
 
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String Title = extras.getString("Title1");
+            String Desc = extras.getString("Desc1");
+
+            collectvalue.setText(Title);
+            collectval.setText(Desc);
+        }
     }
 }
